16d748e76d451fbdd0ef19e38fe39b194cf53c99
deva-18192 <devadharshini.rt@zohocorp.com>
2024-09-25 23:42:29 +0530

    Update Models

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import User
+from .models import MemorialProfile
+from .models import Plan
+from .models import ProfilePermission
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(MemorialProfile)
+admin.site.register(Plan)
+admin.site.register(ProfilePermission)
--- user/models.py
+++ user/models.py
@@ -14,9 +14,36 @@ class User(AbstractUser):
         self.username = self.email
         super(User, self).save(*args, **kwargs)
 
+class Plan(models.Model):
+    name = models.TextField()
+    cost = models.PositiveIntegerField()
+    storage = models.PositiveIntegerField()
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class MemorialProfile(models.Model):
+    title = models.CharField(max_length=200,blank=True)
+    about = models.TextField()
+    owner = models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="associated_memorial")
+    current_storage = models.PositiveIntegerField()
+    current_plan = models.ForeignKey(Plan,on_delete=models.DO_NOTHING,related_name="associated_plan")
+
+    def __str__(self):
+        return self.title
+
+class ProfilePermission(models.Model):
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="associated_user")
+    permission = models.CharField(max_length=20,default="view")
+    memorial_profile = models.ForeignKey(MemorialProfile, on_delete=models.DO_NOTHING, related_name="associated_memorial_profile")
+
+    def __str__(self):
+            return self.user
 
 
-# MemorialProfile  -> owner (User) , title, about  , users(can_edit, can_view) ,current_storage , current_plan
+
+#MemorialProfile  -> owner (User) , title, about  , users(can_edit, can_view) ,current_storage , current_plan
 
 #media - > file , associated_memorial, size
 
